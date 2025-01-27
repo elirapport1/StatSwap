fdefa1fa897b26e4f1f3fa5abe2ad74cbfd1b5bd
elirapport22 <ebbeck4@gmail.com>
2025-01-27 11:53:49 -0600

    repo organization

--- config/vite.config.ts
+++ config/vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
     base: '/StatSwap/', // e.g. '/StatSwap/'
     plugins: [react()],
+    root: path.resolve(__dirname, '..'),
 });
